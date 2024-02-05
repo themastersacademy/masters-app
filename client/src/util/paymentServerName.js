68adce8abdae059656bdb18eec494f3688b3b725
Abishek . A <21ec001@psr.edu.in>
2024-02-05 22:13:30 +0530

    clear cache

--- client/src/util/paymentServerName.js
+++ client/src/util/paymentServerName.js
@@ -2,4 +2,5 @@
 // export  const domainName = 'http://localhost:1338/payment/ccavRequestHandler'  
 
 // production
+
  export  const  domainName = 'https://app.themastersacademy.in/payment/ccavRequestHandler'
